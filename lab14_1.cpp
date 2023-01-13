d7261c127f9a927c774e72f14fd2f3f92bb1a819
hellowrld <jsdajdjjdjsdffsd243535@gmail.com>
2023-01-13 15:37:09 +0700

    Done

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,41 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double d[],int N,double B[]) {
+
+    double sd;
+    double geo = 1;
+    double hamo = 0;
+    double min = 12.5;
+    double max = 0;
+    
+    for(int i=0;i<N;i++) B[0] += d[i];
+
+    B[0] /= N;
+    double mean = B[0];
+
+    for (int i=0;i<N;i++) sd += pow(d[i] - mean,2);
+
+    B[1] = sqrt(sd/N);
+    for (int i=0;i<N;i++) 
+    geo *= d[i];
+
+    B[2] = pow(geo,1.0/N);
+    for(int i=0;i<N;i++)
+    hamo += 1.0/d[i];
+    
+    B[3] = N/hamo;
+    for(int i=0;i<N;i++) {
+        if (d[i] >= max) {
+            max = d[i];
+        } 
+    }
+    B[4] = max;
+        for(int i=0;i<N;i++) {
+        if (d[i] <= min) {
+            min = d[i];
+        } 
+    }
+    B[5] = min;
+}
